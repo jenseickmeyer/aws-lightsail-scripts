a2dd794666556d2335dce322415988b789af5bfb
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2019-05-05 08:44:35 +0200

    Extension of launch script for CodeDeploy install

--- launch.sh
+++ launch.sh
@@ -10,6 +10,25 @@ sudo mount -t nfs4 \
            -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
            172.31.14.222:/ /mnt/efs
 
+# Install CodeDeploy Agent
+yum -y install ruby
+yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-eu-central-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+rm ./install
+
+cat <<EOF > /etc/codedeploy-agent/conf/codedeploy.onpremises.yml
+---
+aws_access_key_id: <ACCESS_KEY>
+aws_secret_access_key: <SECRET_ACCESS_KEY>
+iam_user_arn: <IAM_USER_ARN>
+region: <AWS_REGION>
+EOF
+
+service codedeploy-agent restart
+
 # Install Docker
 yum -y install docker
 service docker start
