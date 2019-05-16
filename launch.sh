46958a537e251b89845d9a9d85b9177a4e48362d
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2019-05-16 06:23:28 +0200

    Mount EFS file system on server launch

--- launch.sh
+++ launch.sh
@@ -3,6 +3,13 @@
 # Install security updates
 yum -y --security update
 
+# Mount shared file system
+yum -y install amazon-efs-utils
+mkdir /mnt/efs
+sudo mount -t nfs4 \
+           -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 \
+           172.31.14.222:/ /mnt/efs
+
 # Install Docker
 yum -y install docker
 service docker start
